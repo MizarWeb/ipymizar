8aca43d59f89f5c4972ef34b37871d17dde0ec3f
lmieulet <leo.mieulet@csgroup.eu>
2021-06-23 17:04:32 +0200

    Remove everything

--- js/src/index.js
+++
@@ -1,4 +0,0 @@
-
-// Export everything from jupyter-mizar and the npm package version number.
-module.exports = require('./jupyter-mizar.js');
-module.exports['version'] = require('../package.json').version;
--- js/src/notebook.js
+++
@@ -1,5 +0,0 @@
-
-// Setup notebook base URL
-__webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/jupyter-mizar/';
-
-module.exports = require('./index.js');
